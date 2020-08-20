7a87092807185f2b2ddec6f2bef3246359ef58fc
Oleg Zavhorodnii <oleggenius95gmail.com>
2020-08-20 20:41:16 +0300

    styled movie info

--- src/Components/MovieInfo/MovieInfo.jsx
+++ src/Components/MovieInfo/MovieInfo.jsx
@@ -6,6 +6,8 @@ import style from "./MovieInfo.module.css";
 
 const MovieInfo = ({ movieInfo }) => {
   const {
+    poster_path: poster,
+    name,
     release_date,
     production_countries,
     title,
@@ -13,54 +15,40 @@ const MovieInfo = ({ movieInfo }) => {
     runtime,
     overview,
   } = movieInfo;
+
   console.log(movieInfo);
+
   return (
     <Container>
-      <section className={style.wrap}>
+      <section className={style.section}>
         <p className={style.title}>{title}</p>
-        <div>
-          <span>{moment(release_date).year()}</span>,
-          <ul>
-            {!!production_countries &&
-              production_countries.map((el, index) => (
-                <li key={index}>{el.iso_3166_1}</li>
-              ))}
-          </ul>
-          <ul>
-            {!!genres && genres.map(({ id, name }) => <li key={id}>{name}</li>)}
-          </ul>
-          <span>{runtime} min</span>
+
+        <img
+          src={poster && `http://image.tmdb.org/t/p/w500/${poster}`}
+          alt={name || title}
+          className={style.poster}
+        />
+
+        <div className={style.container}>
+          <div className={style.parameters}>
+            <span>{moment(release_date).year()},&nbsp;</span>
+            <ul className={style.countriesList}>
+              {!!production_countries &&
+                production_countries.map((el, index) => (
+                  <li key={index}>{el.name},&nbsp;</li>
+                ))}
+            </ul>
+            <ul className={style.generesList}>
+              {!!genres &&
+                genres.map(({ id, name }) => <li key={id}>{name},&nbsp;</li>)}
+            </ul>
+            <span className={style.runtime}>{runtime} min</span>
+          </div>
+          <p className={style.overview}>{overview}</p>
         </div>
-        <p>{overview}</p>
       </section>
     </Container>
   );
 };
 
 export default MovieInfo;
-
-// adult: false
-// backdrop_path: "/t93doi7EzoqLFckidrGGnukFPwd.jpg"
-// belongs_to_collection: null
-// budget: 0
-// genres: (3) [{…}, {…}, {…}]
-// homepage: "https://www.netflix.com/title/80198975"
-// id: 339095
-// imdb_id: "tt1552211"
-// original_language: "en"
-// original_title: "The Last Days of American Crime"
-// overview: "In the not-too-distant future, as a final response to crime and terrorism, the U.S. government plans to broadcast a signal that will make it impossible for anyone to knowingly break the law."
-// popularity: 54.882
-// poster_path: "/ygCQnDEqUEIamBpdQdDYnFfxvgM.jpg"
-// production_companies: (3) [{…}, {…}, {…}]
-// production_countries: [{…}]
-// release_date: "2020-06-05"
-// revenue: 0
-// runtime: 148
-// spoken_languages: [{…}]
-// status: "Released"
-// tagline: ""
-// title: "The Last Days of American Crime"
-// video: false
-// vote_average: 5.7
-// vote_count: 166
